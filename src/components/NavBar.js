255bfbf2774333229fd72358858d7f8776dfd00e
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-14 17:04:32 +0000

    add custom css to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import logo from '../assets/logo.png'
+import styles from '../styles/NavBar.module.css'
 
 
 const NavBar = () => {
     return (
         <div>
-            <Navbar bg="dark" expand="md" fixed='top'>
+            <Navbar className={styles.NavBar} expand="md" fixed='top' >
                 <Container>
                     <Navbar.Brand href="#home"><img src={logo} alt='leaf logo' height={45} /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto text-right text-light">
-                            <Nav.Link><i className="fa-solid fa-house"></i> Home</Nav.Link>
-                            <Nav.Link><i className="fa-solid fa-right-to-bracket"></i> Sign In</Nav.Link>
-                            <Nav.Link><i className="fa-solid fa-user-plus"></i> Sign Up</Nav.Link>
+                    <Navbar.Collapse className="light" id="basic-navbar-nav">
+                        <Nav className={`${styles.Nav} ml-auto text-right`}>
+                            <Nav.Link><span><i className="fa-solid fa-house"></i> Home</span></Nav.Link>
+                            <Nav.Link><span><i className="fa-solid fa-right-to-bracket"></i> Sign In</span></Nav.Link>
+                            <Nav.Link><span><i className="fa-solid fa-user-plus"></i> Sign Up</span></Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
