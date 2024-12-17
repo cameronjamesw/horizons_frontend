f4054bac35308304fad6f19a9ce231f80fe35a89
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-17 23:10:38 +0000

    wire up post-create, add postcreateform

---
+++ src/pages/posts/PostCreateForm.js
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+
+import UploadIcon from "../../assets/upload-icon.jpeg";
+
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import Asset from "../../components/Asset";
+import { Image } from "react-bootstrap";
+
+function PostCreateForm() {
+
+  const [errors, setErrors] = useState({});
+
+  const [formDetail, setFormDetail] = useState({
+    title: "",
+    content: "",
+    image: ""
+  })
+
+  const { title, content, image } = formDetail
+
+  const handleChange = (e) => {
+    setFormDetail({
+      ...formDetail,
+      [e.target.name]: e.target.value
+    })
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setFormDetail({
+        ...formDetail,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+
+  const textFields = (
+    <div className="text-center">
+      <Form>
+        <Form.Group controlId="title">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            name="title"
+            value={title}
+            onChange={handleChange} />
+        </Form.Group>
+
+        <Form.Group controlId="content">
+          <Form.Label>Content</Form.Label>
+          <Form.Control
+            as="textarea"
+            name="content"
+            rows={6}
+            value={content}
+            onChange={handleChange} />
+        </Form.Group>
+      </Form>
+
+
+
+      <Button
+        className={`${btnStyles.Button}`}
+        onClick={() => { }}
+        variant="success"
+      >
+        cancel
+      </Button>
+      <Button
+        className={`${btnStyles.Button}`}
+        type="submit"
+        variant="success">
+        create
+      </Button>
+    </div>
+  );
+
+  return (
+    <Form>
+      <Row>
+        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
+            <Form.Group className="text-center">
+            {image ? (
+                <>
+                  <figure>
+                    <Image className={appStyles.Image} src={image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.Button} ${btnStyles.Green} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
+                <Form.Label
+                  className={`${appStyles.Pointer} d-flex justify-content-center`}
+                  htmlFor="image-upload"
+                >
+                  <Asset
+                    src={UploadIcon}
+                    height={250}
+                    message="Click or tap to upload an image"
+                  />
+                </Form.Label>
+              )}
+
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+              />
+
+            </Form.Group>
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>{textFields}</Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
+
+export default PostCreateForm;
