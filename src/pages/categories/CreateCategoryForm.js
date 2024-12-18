bb38acc511c93c29ba90e47d290a5491af9245b4
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-18 19:57:20 +0000

    create category form, wire up to api

--- src/pages/categories/CreateCategoryForm.js
+++ src/pages/categories/CreateCategoryForm.js
@@ -1,57 +1,84 @@
 import React, { useState } from "react";
 
 import { Form, Alert, Button, Col, Row, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import appStyles from "../../App.module.css";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 
 function CreateCategoryForm() {
 
-  return (
-    <Row className={styles.Row}>
-            <Col className="my-auto p-0 p-md-2" md={6}>
+    const [categoryName, setCategoryName] = useState({
+        name: ""
+    });
+
+    const [errors, setErrors] = useState({})
+
+    const { name } = categoryName;
+    
+    const history = useHistory();
+
+    const handleChange = (e) => {
+        setCategoryName({
+            ...categoryName,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData
+
+        formData.append('name', name)
+
+        try {
+            const { data } = await axiosReq.post('/categories/', formData);
+            history.push('/');
+        } catch (err) {
+            console.log(err)
+            if (err.response?.status !== 401) {
+                setErrors(err.response?.data);
+              }
+        }
+
+    }
+
+    return (
+        <Row className={styles.Row}>
+            <Col className="my-5 p-0 p-md-2" xs={10} lg={6}>
                 <Container className={`${appStyles.Content} p-4 `}>
-                    <h1 className={styles.Header}>sign in</h1>
-                    <Form>
-                        <Form.Group controlId="username">
-                            <Form.Label className="d-none">Username</Form.Label>
+                    <h1 className={styles.Header}>Create Category</h1>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="name">
+                            <Form.Label className="d-none">Category Name</Form.Label>
                             <Form.Control
                                 type="text"
-                                placeholder="Username"
-                                name="username"
+                                placeholder="Enter category name.."
+                                name="name"
+                                value={name}
+                                onChange={handleChange}
                                 className={styles.Input}
                             />
                         </Form.Group>
-
-                        <Form.Group controlId="password">
-                            <Form.Label className="d-none">Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                placeholder="Password"
-                                name="password"
-                                className={styles.Input}
-                            />
-                        </Form.Group>
+                                {errors?.name?.map((message, idx) => (
+                                  <Alert variant="warning" key={idx}>
+                                    {message}
+                                  </Alert>
+                                ))}
 
                         <Button
                             variant="success"
                             type="submit"
                         >
-                            Sign in
+                            Create
                         </Button>
-
                     </Form>
-                </Container>
-                <Container className={`mt-3 ${appStyles.Content}`}>
-                    <Link className={styles.Link} to="/signup">
-                        Don't have an account? <span>Sign up now!</span>
-                    </Link>
                 </Container>
             </Col>
         </Row>
-  )
+    )
 }
 
 export default CreateCategoryForm
