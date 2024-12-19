3daf51880e89a2cbcc80665ca4461b8423430c7c
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-19 20:55:20 +0000

    add post dropdown menu, add deletion functionality

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -5,6 +5,8 @@ import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Avatar from '../../components/Avatar';
 import { axiosRes } from '../../api/axiosDefaults';
+import { MoreDropdown } from '../../components/MoreDropdown';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Post = (props) => {
     const {
@@ -21,12 +23,25 @@ const Post = (props) => {
         image,
         updated_at,
         postPage,
-        category,
         setPosts,
     } = props;
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
+    const history = useHistory();
+
+    const handleEdit = () => {
+        history.push(`/posts/${id}/edit`);
+    };
+
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/posts/${id}/`);
+            history.goBack();
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     const handleLike = async () => {
         try {
@@ -103,7 +118,12 @@ const Post = (props) => {
                     </Link>
                     <div className='d-flex align-items-center'>
                         <span>{updated_at}</span>
-                        {is_owner && postPage && '...'}
+                        {is_owner && postPage && (
+                            <MoreDropdown
+                                handleEdit={handleEdit}
+                                handleDelete={handleDelete}
+                            />
+                        )}
                         {favourite_id ? (
                             <span onClick={handleUnfavourite}>
                                 <i className={`fa-solid fa-star ${styles.StarOutline}`} />
