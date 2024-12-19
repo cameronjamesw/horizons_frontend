b2497ffbf2555019a257b021af1d5dfafc2af333
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-19 01:49:30 +0000

    add post component

---
+++ src/pages/posts/Post.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import styles from '../../styles/Post.module.css'
+import { useCurrentUser } from '../../contexts/CurrentUserContext'
+import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Avatar from '../../components/Avatar';
+
+const Post = (props) => {
+    const {
+        id,
+        owner,
+        profile_id,
+        profile_image,
+        comments_count,
+        likes_count,
+        like_id,
+        favourite_id,
+        title,
+        content,
+        image,
+        updated_at,
+        postPage,
+        category,
+    } = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner
+    return (
+        <Card className={styles.Post}>
+            <Card.Body>
+                <Media className='align-items-center justify-content-between'>
+                    <Link to={`/profiles/${profile_id}/`}>
+                        <Avatar src={profile_image} height={55} />
+                        {owner}
+                    </Link>
+                    <div className='d-flex align-items-center'>
+                        <span>{updated_at}</span>
+                        {is_owner && postPage && '...'}
+                        { favourite_id ? (
+                            <i onClick={() => {}} className={`fa-solid fa-star ${styles.StarOutline}`} />
+                        ) : currentUser ? (
+                            <i onClick={() => {}} className={`fa-regular fa-star ${styles.StarOutline}`} />
+                        ) : (
+                            <span className='d-none'><i className={`fa-regular fa-star ${styles.StarOutline}`} /></span>
+                        )}
+                    </div>
+                </Media>
+            </Card.Body>
+            <Link to={`/posts/${id}/`}>
+                <Card.Img src={image} alt={title} />
+            </Link>
+            <Card.Body>
+                {title && <Card.Title className='text-center'>{title}</Card.Title>}
+                {content && <Card.Text>{content}</Card.Text>}
+                <div className={styles.PostBar}>
+          {is_owner ? (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>You can't like your own post!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          ) : like_id ? (
+            <span onClick={() => {}}>
+              <i className={`fas fa-heart ${styles.Heart}`} />
+            </span>
+          ) : currentUser ? (
+            <span onClick={() => {}}>
+              <i className={`far fa-heart ${styles.HeartOutline}`} />
+            </span>
+          ) : (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>Log in to like posts!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          )}
+          {likes_count}
+          <Link to={`/posts/${id}`}>
+            <i className="far fa-comments" />
+          </Link>
+          {comments_count}
+        </div>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Post
