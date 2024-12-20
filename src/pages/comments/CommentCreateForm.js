ff4cfaad912de8e65e2c9e49259e5bebe37e341e
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-20 20:04:56 +0000

    add create comment functionality

---
+++ src/pages/comments/CommentCreateForm.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+import styles from "../../styles/CommentCreateEditForm.module.css";
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+import { Alert } from "react-bootstrap";
+
+function CommentCreateForm(props) {
+    const { post, setPost, setComments, profileImage, profile_id } = props;
+    const [content, setContent] = useState("");
+
+    const [errors, setErrors] = useState({})
+
+    const handleChange = (event) => {
+        setContent(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const { data } = await axiosRes.post("/comments/", {
+                content,
+                post,
+            });
+            setComments((prevComments) => ({
+                ...prevComments,
+                results: [data, ...prevComments.results],
+            }));
+            setPost((prevPost) => ({
+                results: [
+                    {
+                        ...prevPost.results[0],
+                        comments_count: prevPost.results[0].comments_count + 1,
+                    },
+                ],
+            }));
+            setContent("");
+        } catch (err) {
+            console.log(err);
+            if (err.response?.status !== 401) {
+                setErrors(err.response?.data);
+            }
+        }
+    };
+
+    return (
+        <Form className="mt-2" onSubmit={handleSubmit}>
+            <Form.Group>
+                <InputGroup>
+                    <Link to={`/profiles/${profile_id}`}>
+                        <Avatar src={profileImage} />
+                    </Link>
+                    <Form.Control
+                        className={styles.Form}
+                        placeholder="my comment..."
+                        as="textarea"
+                        value={content}
+                        onChange={handleChange}
+                        rows={2}
+                    />
+                </InputGroup>
+            </Form.Group>
+            {errors?.content?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+            <button
+                className={`${styles.Button} btn d-block ml-auto`}
+                disabled={!content.trim()}
+                type="submit"
+            >
+                post
+            </button>
+        </Form>
+    );
+}
+
+export default CommentCreateForm;
