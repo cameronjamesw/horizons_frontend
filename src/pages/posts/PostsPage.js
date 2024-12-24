e5f4176bfe550da9102870ad8b5a15e7909f3a29
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-24 18:08:29 +0000

    add basic functionality to popular profiles component

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -15,6 +15,7 @@ import Post from "./Post";
 import NoResults from "../../assets/resetti.png"
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function PostsPage({message, filter=""}) {
     const [posts, setPosts] = useState({ results: [] });
@@ -89,7 +90,7 @@ function PostsPage({message, filter=""}) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <PopularProfiles />
       </Col>
     </Row>
   );
