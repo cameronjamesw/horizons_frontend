243c7a496866e5859a97e7560965fb6a93ea07e4
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-26 13:11:30 +0000

    add category search button and styling

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -7,6 +7,7 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
+import btnStyles from "../../styles/Button.module.css"
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 import Asset from "../../components/Asset";
@@ -16,11 +17,13 @@ import NoResults from "../../assets/resetti.png"
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
+import { Dropdown } from "react-bootstrap";
 
-function PostsPage({message, filter=""}) {
-    const [posts, setPosts] = useState({ results: [] });
+function PostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
+  const [categories, setCategories] = useState();
 
   const [query, setQuery] = useState("");
 
@@ -45,37 +48,66 @@ function PostsPage({message, filter=""}) {
       clearTimeout(timer);
     };
   }, [filter, pathname, query]);
-  
+
+  useEffect(() => {
+          const handleMount = async () => {
+              try {
+                  const [{ data: categories }] = await Promise.all([
+                      axiosReq.get('/categories/')
+                  ])
+                  setCategories(categories.results)
+              } catch (err) {
+                  console.log(err)
+              }
+          }
+          handleMount();
+      }, [])
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <PopularProfiles mobile />
         <i className={`fas fa-search ${styles.SearchIcon}`} />
-        <Form
-          className={styles.SearchBar}
-          onSubmit={(event) => event.preventDefault()}
-        >
-          <Form.Control
-            value={query}
-            onChange={(event) => setQuery(event.target.value)}
-            type="text"
-            className="mr-sm-2"
-            placeholder="Search posts"
-          />
-        </Form>
+        <div className="d-flex">
+          <Form
+            className={`w-75 ${styles.SearchBar}`}
+            onSubmit={(event) => event.preventDefault()}
+          >
+            <Form.Control
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+              type="text"
+              className="mr-sm-2"
+              placeholder="Search posts"
+            />
+          </Form>
+          <Dropdown className="ml-auto">
+            <Dropdown.Toggle className={`${btnStyles.CatButton} ${btnStyles.Green} btn-lg`}>
+              Categories
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu className={styles.DropDown}>
+            {categories?.map((categoryObj) => (
+                            <Dropdown.Item className={styles.DropDownItem} key={categoryObj.id} href={`categories/${categoryObj.id}/`}>
+                                {categoryObj.name}
+                            </Dropdown.Item>
+                        ))}
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
         {hasLoaded ? (
           <>
             {posts.results.length ? (
               <InfiniteScroll
-              children={
-                posts.results.map((post) => (
-                  <Post key={post.id} {...post} setPosts={setPosts} />
-                ))
-              }
-              dataLength={posts.results.length}
-              loader={<Asset spinner />}
-              hasMore={!!posts.next}
-              next={() => fetchMoreData(posts, setPosts)}
+                children={
+                  posts.results.map((post) => (
+                    <Post key={post.id} {...post} setPosts={setPosts} />
+                  ))
+                }
+                dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => fetchMoreData(posts, setPosts)}
               />
             ) : (
               <Container className={appStyles.Content}>
